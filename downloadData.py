d0feeb20b8a5d93218e2cf0391e9fd90becc1283
Yugesh <77796731+yugesharma@users.noreply.github.com>
2024-12-18 18:07:53 +0530

    Updated downloadData.py
    Specified timezone

--- downloadData.py
+++ downloadData.py
@@ -1,14 +1,16 @@
 import requests
 import datetime  
+import pytz
 import xarray as xr
 import pandas as pd
 import numpy as np
 
 
 
-today=datetime.datetime.today()
+myTimeZone=pytz.timezone('Asia/Kolkata')
+today=datetime.datetime.now(myTimeZone)
 today=today.date()
-endDate=today+datetime.timedelta(days=6)
+endDate=today+datetime.timedelta(days=8)
 endDate=endDate
 url=f'https://pae-paha.pacioos.hawaii.edu/thredds/ncss/ncep_global/NCEP_Global_Atmospheric_Model_best.ncd?var=ugrd10m&var=vgrd10m&north=50&west=278&east=345&south=12&disableLLSubset=on&disableProjSubset=on&horizStride=1&time_start={today}T12%3A00%3A00Z&time_end={endDate}T12%3A00%3A00Z&timeStride=1&addLatLon=true'
 print(url)
@@ -25,11 +27,11 @@ else:
 ds=xr.open_dataset('forecast1.nc')
 df=ds.to_dataframe()
 
-df.to_csv('forecast1.csv')
+df.to_csv('forecast.csv')
 
 
 # Load the CSV file into a Pandas DataFrame
-input_file = 'forecast1.csv'
+input_file = 'forecast.csv'
 df = pd.read_csv(input_file)
 
 df['longitude']=((df['longitude'])+180)%360 - 180
